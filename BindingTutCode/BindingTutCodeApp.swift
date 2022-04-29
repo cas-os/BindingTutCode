368cccc21ab80a8fbe9b40b9e59029c841964ad1
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-29 11:35:03 +0200

    Initial Commit

---
+++ BindingTutCode/BindingTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingTutCodeApp.swift
+//  BindingTutCode
+//
+//  Created by HAL-9001 on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BindingTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
